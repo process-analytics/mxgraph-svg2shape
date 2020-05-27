7208b4e17ddd865560a0c66046a8764c6b6d0c9f
Marcin Michniewicz <45601541+aibcmars@users.noreply.github.com>
2020-05-27 15:12:11 +0200

    Add support for arc - tests

--- src/test/java/com/mxgraph/xml2js/Xml2JsTest.java
+++ src/test/java/com/mxgraph/xml2js/Xml2JsTest.java
@@ -14,6 +14,7 @@ class Xml2JsTest {
                 .element("<move x='141.9' y='0'/>")
                 .element("<curve x1='85.52' x2='39.66' x3='39.66' y1='0' y2='45.87' y3='102.25'/>")
                 .element("<line x='39.66' y='131.4'/>")
+                .element("<arc large-arc-flag='0' rx='6.43' ry='6.43' sweep-flag='1' x='95.51' x-axis-rotation='0' y='135.03'/>")
                 .element("<close/>")
                 .end()
         ;
@@ -25,6 +26,7 @@ class Xml2JsTest {
                 "canvas.moveTo(141.9, 0);",
                 "canvas.curveTo(85.52, 0, 39.66, 45.87, 39.66, 102.25);",
                 "canvas.lineTo(39.66, 131.4);",
+                "canvas.arcTo(6.43, 6.43, 0, 0, 1, 95.51, 135.03);",
                 "canvas.close();",
                 "canvas.fillAndStroke();"
         );
